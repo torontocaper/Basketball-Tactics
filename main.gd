984259f3f862b5ec5abdc9fdc1d45a471e0e108c
Adam Burns <torontocaper@gmail.com>
2024-02-27 21:10:09 -0500

    added 'distance_to_origin' calculation to main.gd

--- main.gd
+++ main.gd
@@ -17,4 +17,9 @@ func _on_round_over(round_number):
 
 func _print_locations():
 	for player in players:
-		print(player.character_name + " is at " + str(player.position) + ", which is " + str(court.local_to_map(player.position)) + " in map space.")
+		var map_position = Vector2(court.local_to_map(player.position))
+		var distance_to_origin = map_position.distance_to(Vector2.ZERO)
+		print(player.character_name + " is at " + str(player.position) + ", which is " + str(map_position) + " in map space.")
+		print("That's approximately " + str(round(distance_to_origin)) + " away from the origin.")
+		
+		
